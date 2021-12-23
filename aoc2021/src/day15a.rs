94e8de0908efb0e01afd371013cc5ad27a9947f0
Chandler Swift <chandler@chandlerswift.com>
2021-12-23 14:01:39 -0600

    Clean up 15a

--- aoc2021/src/day15a.rs
+++ aoc2021/src/day15a.rs
@@ -1,38 +1,17 @@
 use std::env;
 use std::fs;
 use std::fmt;
-use std::cmp::Ordering;
 use std::collections::HashMap;
 
-#[derive(Eq, Clone, Hash, Debug)]
+#[derive(Clone, Hash, Debug)]
 struct Vertex {
     x: i32,
     y: i32,
-    distance_from_start: u32,
-}
-
-impl Ord for Vertex {
-    fn cmp(&self, other: &Self) -> Ordering {
-        // TODO: is this correct or backwards?
-        self.distance_from_start.cmp(&other.distance_from_start)
-    }
-}
-
-impl PartialOrd for Vertex {
-    fn partial_cmp(&self, other: &Self) -> Option<Ordering> {
-        Some(self.cmp(other))
-    }
-}
-
-impl PartialEq for Vertex {
-    fn eq(&self, other: &Self) -> bool {
-        self.x == other.x && self.y == other.y
-    }
 }
 
 impl fmt::Display for Vertex {
     fn fmt(&self, f: &mut fmt::Formatter) -> fmt::Result {
-        write!(f, "({}, {} ({}))", self.x, self.y, self.distance_from_start)
+        write!(f, "({}, {})", self.x, self.y)
     }
 }
 
@@ -40,7 +19,7 @@ fn find_best_path_risk(grid: Vec<Vec<u8>>) -> u32 {
     // Using Dijkstra's algorithm
     let height = grid.len() as i32;
     let width = grid[0].len() as i32;
-    println!("Width x height: {}x{}", width, height);
+
     let mut dist = vec![vec![u32::MAX; width as usize]; height as usize];
     let mut prev: Vec<Vec<Option<Vertex>>> = vec![vec![None; width as usize]; height as usize];
 
@@ -50,55 +29,47 @@ fn find_best_path_risk(grid: Vec<Vec<u8>>) -> u32 {
 
     for (i, row) in grid.iter().enumerate() {
         for (j, _cell) in row.iter().enumerate() {
-            println!("Inserting at {},{}", j, i);
             q.insert((j, i), Vertex{
                 x: j as i32,
                 y: i as i32,
-                distance_from_start: u32::MAX,
             });
         }
     }
-    {
-        let mut a = q.get_mut(&(0,0)).unwrap();
-        a.distance_from_start = 0;
-        dist[0][0] = 0;
-    }
+
+    // Set the cost of the starting cell to 0
+    dist[0][0] = 0;
 
     while q.len() > 0 {
-        // println!("{:?}", q);
-
         // find the closest element still in the list
-        let mut min_vertex = q.values().next().unwrap().clone(); // Pick an element, any element
-        let mut min_vertex_coords = (0, 0);
-        for (coords, vertex) in &q {
-            if *vertex <= min_vertex {
-                min_vertex = vertex.clone();
-                min_vertex_coords = *coords;
+        let mut min_vertex_cost = u32::MAX;
+        let mut min_vertex_coords = None;
+        for coords in q.keys() {
+            if dist[coords.1][coords.0] < min_vertex_cost {
+                min_vertex_cost = dist[coords.1][coords.0];
+                min_vertex_coords = Some(coords.clone());
             }
         }
-        // println!("Removing {:?}", min_vertex_coords);
-        let u = q.remove(&min_vertex_coords).unwrap();
-        println!("Removed {}", u);
+        let u = q.remove(&min_vertex_coords.unwrap()).unwrap();
 
         // print out the contents of prev
-        for (i, row) in prev.iter().enumerate() {
-            for (j, cell) in row.iter().enumerate() {
-                if *cell == None {
-                    print!(".");
-                } else if i < cell.as_ref().unwrap().y as usize {
-                    print!("v");
-                } else if i > cell.as_ref().unwrap().y as usize {
-                    print!("^");
-                } else if j < cell.as_ref().unwrap().x as usize {
-                    print!(">");
-                } else if j > cell.as_ref().unwrap().x as usize {
-                    print!("<");
-                } else {
-                    print!("*");
-                }
-            }
-            println!();
-        }
+        // for (i, row) in prev.iter().enumerate() {
+        //     for (j, cell) in row.iter().enumerate() {
+        //         if cell.is_none() {
+        //             print!(".");
+        //         } else if i < cell.as_ref().unwrap().y as usize {
+        //             print!("v");
+        //         } else if i > cell.as_ref().unwrap().y as usize {
+        //             print!("^");
+        //         } else if j < cell.as_ref().unwrap().x as usize {
+        //             print!(">");
+        //         } else if j > cell.as_ref().unwrap().x as usize {
+        //             print!("<");
+        //         } else {
+        //             print!("*");
+        //         }
+        //     }
+        //     println!();
+        // }
 
         if u.x == width - 1 && u.y == height - 1 { // It's our target vertex
 
@@ -106,41 +77,36 @@ fn find_best_path_risk(grid: Vec<Vec<u8>>) -> u32 {
             let mut total = 0;
             while !(current.x == 0 && current.y == 0) { // until we've gotten back to the beginning
                 total += grid[current.y as usize][current.x as usize] as u32;
-                // println!("{}", grid[current.y as usize][current.x as usize] as u32);
                 current = Vertex{
                     x: prev[current.y as usize][current.x as usize].as_ref().unwrap().x,
                     y: prev[current.y as usize][current.x as usize].as_ref().unwrap().y,
-                    distance_from_start: 0,
                 };
             }
             return total;
         }
 
-
         let adjacent_vertices = [
-            // We're not using distance_from_start here, so /shrug
-            Vertex{x: u.x-1, y: u.y, distance_from_start: u32::MAX},
-            Vertex{x: u.x+1, y: u.y, distance_from_start: u32::MAX },
-            Vertex{x: u.x, y: u.y-1, distance_from_start: u32::MAX },
-            Vertex{x: u.x, y: u.y+1, distance_from_start: u32::MAX },
+            Vertex{x: u.x-1, y: u.y},
+            Vertex{x: u.x+1, y: u.y},
+            Vertex{x: u.x, y: u.y-1},
+            Vertex{x: u.x, y: u.y+1},
             ];
         for v in adjacent_vertices {
-            // println!("Checking {},{}", v.x, v.y);
+            // If it's not out of bounds and is still in our list of vertices to check
             if v.x >= 0 && v.x < width && v.y >= 0 && v.y < height && q.contains_key(&(v.x as usize, v.y as usize)) {
-                // println!("{} {} {} {}", u, v, dist[u.y as usize][u.x as usize], grid[v.y as usize][v.x as usize]);
+                // alt is the distance it would cost to travel this alternative path
                 let alt = dist[u.y as usize][u.x as usize] + grid[v.y as usize][v.x as usize] as u32;
+                // if it's better than the best path we currently have, take it!
                 if alt < dist[v.y as usize][v.x as usize] {
-                    // println!("Setting {},{} to {}", v.x, v.y, alt);
-                    let real_v = q.get_mut(&(v.x as usize, v.y as usize)).unwrap();
-                    real_v.distance_from_start = alt;
                     dist[v.y as usize][v.x as usize] = alt;
                     prev[v.y as usize][v.x as usize] = Some(u.clone());
                 }
             }
         }
-        // println!("{} remaining in Q", q.len());
     }
 
+    // We should have returned when we hit the fastest solution above. Clearly,
+    // we didn't. This should indicate an error in the code.
     panic!("Didn't find a solution!");
 }
 
