ad08b2de6a41949b06222d55098380968c942e62
Chandler Swift <chandler@chandlerswift.com>
2022-12-08 00:36:53 -0600

    Update day6.rs for part 2

--- 2022/day6/day6.rs
+++ 2022/day6/day6.rs
@@ -1,16 +1,14 @@
 use std::fs;
 
-fn process(data: &str) -> usize {
+fn process(data: &str, len: usize) -> usize {
     let chars: Vec<char> = data.chars().collect();
-    for i in 4..chars.len() {
-        if chars[i] != chars[i - 1]
-            && chars[i] != chars[i - 2]
-            && chars[i] != chars[i - 3]
-            && chars[i - 1] != chars[i - 2]
-            && chars[i - 1] != chars[i - 3]
-            && chars[i - 2] != chars[i - 3]
-        {
-            return i + 1;
+    for i in len..chars.len() {
+        let mut chars = chars[i-len..i].to_vec(); // TODO: this seems unidiomatic?
+        chars.sort();
+        chars.dedup();
+        if chars.len() == len {
+            // no duplicates
+            return i;
         }
     }
     panic!("No valid marker found");
@@ -19,14 +17,14 @@ fn process(data: &str) -> usize {
 fn main() {
     let data = fs::read_to_string("input.txt").unwrap();
     let data = data.trim();
-    println!("{}", process(data));
+    println!("{}", process(data, 14)); // 4 for part A
 }
 
 #[cfg(test)]
 mod test {
     use super::*;
     #[test]
-    fn test() {
+    fn test_short() {
         let data = vec![
             ("mjqjpqmgbljsphdztnvjfqwrcgsmlb", 7),
             ("bvwbjplbgvbhsrlpgdmjqwftvncz", 5),
@@ -35,8 +33,20 @@ mod test {
             ("zcfzfwzzqfrljwzlrfnpqdbhtmscgvjw", 11),
         ];
         for (datum, result) in data {
-	    println!("{}: expected {}, got {}", datum, result, process(datum));
-            assert!(process(datum) == result);
+            assert!(process(datum, 4) == result);
+        }
+    }
+    #[test]
+    fn test_long() {
+        let data = vec![
+            ("mjqjpqmgbljsphdztnvjfqwrcgsmlb", 19),
+            ("bvwbjplbgvbhsrlpgdmjqwftvncz", 23),
+            ("nppdvjthqldpwncqszvftbrmjlhg", 23),
+            ("nznrnfrfntjfmvfwmzdfjlvtqnbhcprsg", 29),
+            ("zcfzfwzzqfrljwzlrfnpqdbhtmscgvjw", 26),
+        ];
+        for (datum, result) in data {
+            assert!(process(datum, 14) == result);
         }
     }
 }
