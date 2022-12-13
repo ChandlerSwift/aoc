3ef0b80832698ae466f3662494d629e608c9e893
Chandler Swift <chandler@chandlerswift.com>
2022-12-12 18:29:43 -0600

    rustfmt day8

--- 2022/day8/day8.rs
+++ 2022/day8/day8.rs
@@ -4,31 +4,31 @@ fn is_visible(forest: &Vec<Vec<u8>>, row: usize, column: usize) -> bool {
     let height = forest[row][column];
     let mut hidden_from_north = false;
     for i in 0..row {
-	if forest[i][column] >= height {
-	    hidden_from_north = true;
-	    break;
-	}
+        if forest[i][column] >= height {
+            hidden_from_north = true;
+            break;
+        }
     }
     let mut hidden_from_south = false;
-    for i in row+1..forest.len() {
-	if forest[i][column] >= height {
-	    hidden_from_south = true;
-	    break;
-	}
+    for i in row + 1..forest.len() {
+        if forest[i][column] >= height {
+            hidden_from_south = true;
+            break;
+        }
     }
     let mut hidden_from_west = false;
     for i in 0..column {
-	if forest[row][i] >= height {
-	    hidden_from_west = true;
-	    break;
-	}
+        if forest[row][i] >= height {
+            hidden_from_west = true;
+            break;
+        }
     }
     let mut hidden_from_east = false;
-    for i in column+1..forest[0].len() {
-	if forest[row][i] >= height {
-	    hidden_from_east = true;
-	    break;
-	}
+    for i in column + 1..forest[0].len() {
+        if forest[row][i] >= height {
+            hidden_from_east = true;
+            break;
+        }
     }
     !hidden_from_north || !hidden_from_south || !hidden_from_west || !hidden_from_east
 }
@@ -36,19 +36,19 @@ fn is_visible(forest: &Vec<Vec<u8>>, row: usize, column: usize) -> bool {
 fn process(data: &str) -> u32 {
     let mut forest: Vec<Vec<u8>> = Vec::new();
     for line in data.split("\n") {
-	let mut tree_row = Vec::new();
-	for c in line.chars() {
-	    tree_row.push(c.to_digit(10).unwrap() as u8);
-	}
-	forest.push(tree_row);
+        let mut tree_row = Vec::new();
+        for c in line.chars() {
+            tree_row.push(c.to_digit(10).unwrap() as u8);
+        }
+        forest.push(tree_row);
     }
     let mut visible_count = 0;
     for row in 0..forest.len() {
-	for column in 0..forest[0].len() {
-	    if is_visible(&forest, row, column) {
-		visible_count += 1;
-	    }
-	}
+        for column in 0..forest[0].len() {
+            if is_visible(&forest, row, column) {
+                visible_count += 1;
+            }
+        }
     }
     visible_count
 }
@@ -69,9 +69,9 @@ mod test {
 65332
 33549
 35390";
-    
+
     #[test]
-    fn test(){
-	assert!(process(DATA) == 21);
+    fn test() {
+        assert!(process(DATA) == 21);
     }
 }
--- 2022/day8/day8b.rs
+++ 2022/day8/day8b.rs
@@ -4,31 +4,31 @@ fn scenic_score(forest: &Vec<Vec<u8>>, row: usize, column: usize) -> u32 {
     let height = forest[row][column];
     let mut visible_to_north = 0;
     for i in 1..=row {
-	visible_to_north += 1;
-	if forest[row-i][column] >= height {
-	    break;
-	}
+        visible_to_north += 1;
+        if forest[row - i][column] >= height {
+            break;
+        }
     }
     let mut visible_to_south = 0;
-    for i in row+1..forest.len() {
-	visible_to_south += 1;
-	if forest[i][column] >= height {
-	    break;
-	}
+    for i in row + 1..forest.len() {
+        visible_to_south += 1;
+        if forest[i][column] >= height {
+            break;
+        }
     }
     let mut visible_to_west = 0;
     for i in 1..=column {
-	visible_to_west += 1;
-	if forest[row][column-i] >= height {
-	    break;
-	}
+        visible_to_west += 1;
+        if forest[row][column - i] >= height {
+            break;
+        }
     }
     let mut visible_to_east = 0;
-    for i in column+1..forest[0].len() {
-	visible_to_east += 1;
-	if forest[row][i] >= height {
-	    break;
-	}
+    for i in column + 1..forest[0].len() {
+        visible_to_east += 1;
+        if forest[row][i] >= height {
+            break;
+        }
     }
     visible_to_north * visible_to_south * visible_to_west * visible_to_east
 }
@@ -36,19 +36,19 @@ fn scenic_score(forest: &Vec<Vec<u8>>, row: usize, column: usize) -> u32 {
 fn process(data: &str) -> u32 {
     let mut forest: Vec<Vec<u8>> = Vec::new();
     for line in data.split("\n") {
-	let mut tree_row = Vec::new();
-	for c in line.chars() {
-	    tree_row.push(c.to_digit(10).unwrap() as u8);
-	}
-	forest.push(tree_row);
+        let mut tree_row = Vec::new();
+        for c in line.chars() {
+            tree_row.push(c.to_digit(10).unwrap() as u8);
+        }
+        forest.push(tree_row);
     }
     let mut max = 0;
     for row in 0..forest.len() {
-	for column in 0..forest[0].len() {
-	    if scenic_score(&forest, row, column) > max {
-		max = scenic_score(&forest, row, column);
-	    }
-	}
+        for column in 0..forest[0].len() {
+            if scenic_score(&forest, row, column) > max {
+                max = scenic_score(&forest, row, column);
+            }
+        }
     }
     max
 }
@@ -69,9 +69,9 @@ mod test {
 65332
 33549
 35390";
-    
+
     #[test]
-    fn test(){
-	assert!(process(DATA) == 8);
+    fn test() {
+        assert!(process(DATA) == 8);
     }
 }
